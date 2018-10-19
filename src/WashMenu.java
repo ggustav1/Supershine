5caaf46898261df903e0bc2dbc53f7de821864e6
Illumias <oliver.varnild@live.dk>
2018-10-19 12:12:57 +0200

    Q

--- src/WashMenu.java
+++ src/WashMenu.java
@@ -2,5 +2,6 @@ public class WashMenu {
     public static void main(String[] args) {
         System.out.println("Car Wash!");
         System.out.println("Fuck bitches");
+        //Get Money
     }
 }
