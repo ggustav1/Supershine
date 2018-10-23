e1c8ac29fb745f6c5e6ad7af60cc98ec7f2a9988
ChristianBendtsen <chri04cm@stud.kea.dk>
2018-10-23 14:17:25 +0200

    hej

--- src/Admin.java
+++ src/Admin.java
@@ -1,4 +1,25 @@
+<<<<<<< Updated upstream
 public class Admin
 {
+=======
+public class Admin {
+
+    private string ID;
+    private int PIN;
+
+    public boolean validateUser (string id, int pin)
+    {
+        if(id == ID && pin == PIN)
+        {
+            return true;
+        }
+        else {
+            return false;
+         }
+    }
+
+    public void displayStatistics()
+
+>>>>>>> Stashed changes
 
 }
