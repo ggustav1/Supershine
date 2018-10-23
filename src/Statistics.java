9154a60823c4fe8cee6d13de464f817da118232f
RasmusLCh <RasmusLCh@users.noreply.github.com>
2018-10-23 14:20:46 +0200

    WashType

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+
 public class Statistics {
+    private ArrayList<CompletedWash> listOfCompletedWashes = new ArrayList<>();
+
+    public float getMoney() {
+        return 1;
+    }
+
+    public int getUserDiscount() {
+        return 2;
+    }
+
+    public int getTotalWashes() {
+        return 3;
+    }
+
+    public int getAmountOfWashes(WashType washtype) {
+        return 4;
+    }
+
 }
