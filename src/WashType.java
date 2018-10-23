89989bf731527c5a1192f6e340a4ff2d4b8eafdc
RasmusLCh <RasmusLCh@users.noreply.github.com>
2018-10-23 14:07:44 +0200

    WashType

--- src/WashType.java
+++ src/WashType.java
@@ -1,2 +1,22 @@
 public class WashType {
+    public String name;
+    private float price;
+    public float time;
+    public boolean earlybird;
+
+    public WashType(String name, float price, float time, boolean earlybird) {
+        this.name = name;
+        this.price = price;
+        this.time = price;
+        this.earlybird = earlybird;
+    }
+
+    public float getPrice() {
+        // TODO Noget med early bird
+        return price;
+    }
+
+    public void addToCompletedWashList() {
+
+    }
 }
