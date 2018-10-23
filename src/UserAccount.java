bd0ff0bd535cd7c49b9aae5c5fde59c07fae5e96
Illumias <oliver.varnild@live.dk>
2018-10-23 13:50:48 +0200

    1st commit

--- src/UserAccount.java
+++ src/UserAccount.java
@@ -1,2 +1,22 @@
 public class UserAccount {
+    private int ID;
+    private int PIN;
+    private float AccountBalance;
+
+    public boolean ValidateUser(int id, int pin){
+        if(id == ID && pin == PIN){
+            return true;
+        } else  {
+            return false;
+        }
+    }
+
+    public float GetAccountBalance(){
+        return AccountBalance;
+    }
+
+    public void ChangeAccountBalance(float BalanceChange){
+        AccountBalance += BalanceChange;
+    }
+
 }
