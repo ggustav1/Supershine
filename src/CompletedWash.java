515f213794cca94ae26293dc7ab5ae45b2890b5e
Illumias <oliver.varnild@live.dk>
2018-10-23 14:10:24 +0200

    1st commit

--- src/CompletedWash.java
+++ src/CompletedWash.java
@@ -1,2 +1,13 @@
+import java.time.*;
+
 public class CompletedWash {
+    private WashType type;
+    private LocalDateTime dateAndTime;
+    private boolean discountUsed;
+
+    CompletedWash(WashType type, LocalDateTime date, boolean useOfDiscount){
+        this.type = type;
+        this.dateAndTime = date;
+        this.discountUsed = useOfDiscount;
+    }
 }
