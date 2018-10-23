410f262e0e20a03397456e9e57baaaf45b6e4f49
Gustav <gust2968@stud.kea.dk>
2018-10-23 14:14:12 +0200

    WashMenu

--- src/WashMenu.java
+++ src/WashMenu.java
@@ -1,4 +1,12 @@
 public class WashMenu {
+    private boolean inUse;
+    private WashType listOfWashType[];
+
+    public void displayMenu(){}
+    public void displayWashTypes(){}
+    public void initiateWash(WashType washType){};
+    public void endService(){}
+
     public static void main(String[] args) {
 
     }
